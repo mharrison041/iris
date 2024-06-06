f0e3974dfe1a3e3d1b4f8d5277fb057a00a5a830
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-06 05:51:10 -0600

    Added abstract dictionary class

--- steno_keyboard/steno_keyboard.ino
+++ steno_keyboard/steno_keyboard.ino
@@ -1,12 +1,24 @@
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
+
+// Classes
+class Dictionary {
+public:
+  virtual bool open() = 0;
+
+  virtual void close() = 0;
+
+  virtual bool seekTextFor(uint8_t steno[]) = 0;
+
+  virtual bool hasNext() = 0;
+
+  virtual uint8_t next() = 0;
+};
 
 // Functions for testing
 void testSuite(char testSuiteName[]) {
