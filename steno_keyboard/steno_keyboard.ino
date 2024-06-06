c92e122c5c6ea199270524b8f317a2968df375c3
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-06 05:43:51 -0600

    Added functions for testing to steno_keyboard.ino

--- steno_keyboard/steno_keyboard.ino
+++ steno_keyboard/steno_keyboard.ino
@@ -7,3 +7,27 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// Functions for testing
+void testSuite(char testSuiteName[]) {
+  Serial.println(testSuiteName);
+}
+
+void test(char testName[]) {
+  Serial.print("    ");
+  Serial.println(testName);
+}
+
+void assertTrue(bool value) {
+  Serial.print("    - ");
+  if (value == true) {
+    Serial.println("Passed");
+  } else {
+    Serial.println("Failed");
+  }
+  Serial.print("\n");
+}
+
+void assertFalse(bool value) {
+  assertTrue(!value);
+}
