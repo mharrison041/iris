9e1b49902aabccb10be175db0720a307c285f61a
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-06 11:39:13 -0600

    Added function to test the SD card dictionary class to its own ino file

---
+++ steno_keyboard/sd_card_dictionary_test.ino
@@ -0,0 +1,12 @@
+#include <SPI.h>
+#include <SD.h>
+
+void testSDCardDictionary() {
+  testSuite("SDCardDictionaryTest");
+
+  uint8_t slaveSelectPin = 4;
+  if (!SD.begin(slaveSelectPin)) {
+    Serial.println("    Failed to connect to SD card");
+    return;
+  }
+}
