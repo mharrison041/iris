62fdb4421ebd69882c6f1c5e5b8369583bb3b8d9
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-07 13:38:01 -0600

    Tested if next() in SDCardDictionary returns the second byte of the text that was looked up

--- steno_keyboard/sd_card_dictionary_test.ino
+++ steno_keyboard/sd_card_dictionary_test.ino
@@ -79,4 +79,49 @@ void testSDCardDictionary() {
     dictionary.close();
     SD.remove(fileName);
   }
+
+  test("next_returnsFirstByteFromText_whenFirstCalled");
+  {
+    uint8_t steno[3] = { 0, 0, 4 };
+    char fileName[] = "test";
+    File file = SD.open(fileName, FILE_WRITE);
+    uint8_t data[16] = { 0, 0, 0, 1,   // number of steno entries
+                         0, 0, 4,      // steno
+                         0, 0, 0, 15,  // initial index of text
+                         0, 0, 0, 16,  // final index of text
+                         17 };         // text
+    file.write(data, 16);
+    file.close();
+    SDCardDictionary dictionary(fileName);
+    dictionary.open();
+    dictionary.seekTextFor(steno);
+
+    assertTrue(dictionary.next() == 17);
+
+    dictionary.close();
+    SD.remove(fileName);
+  }
+
+  test("next_returnsSecondByteFromText_whenCalledTwice");
+  {
+    uint8_t steno[3] = { 0, 0, 4 };
+    char fileName[] = "test";
+    File file = SD.open(fileName, FILE_WRITE);
+    uint8_t data[17] = { 0, 0, 0, 1,   // number of steno entries
+                         0, 0, 4,      // steno
+                         0, 0, 0, 15,  // initial index of text
+                         0, 0, 0, 17,  // final index of text
+                         17, 50 };     // text
+    file.write(data, 17);
+    file.close();
+    SDCardDictionary dictionary(fileName);
+    dictionary.open();
+    dictionary.seekTextFor(steno);
+    dictionary.next();
+
+    assertTrue(dictionary.next() == 50);
+
+    dictionary.close();
+    SD.remove(fileName);
+  }
 }
