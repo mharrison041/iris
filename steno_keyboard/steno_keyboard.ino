9edc2bde2345c98fe32f05773a294e93fa14641c
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-09 03:45:16 -0600

    Added abstract ChordDetector

--- steno_keyboard/steno_keyboard.ino
+++ steno_keyboard/steno_keyboard.ino
@@ -15,6 +15,15 @@ public:
   virtual void read(uint8_t keyStates[]) = 0;
 };
 
+class ChordDetector {
+public:
+  virtual void scan(uint8_t keyStates[]) = 0;
+
+  virtual bool detectedChord() = 0;
+  
+  virtual void get(uint8_t chord[]) = 0;
+};
+
 class Dictionary {
 public:
   virtual bool open() = 0;
