85f0e1ad41499e9350aa47a1503204657c7b7c22
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-18 05:56:07 -0600

    Tested that SDCardText is initializable

--- steno_keyboard/SDCardText.h
+++ steno_keyboard/SDCardText.h
@@ -1,9 +1,12 @@
 #pragma once
-
+#include <SPI.h>
+#include <SD.h>
 #include "Text.h"
 
 class SDCardText : public Text {
 public:
+  SDCardText(uint32_t initialPosition, uint32_t finalPosition, File file) {}
+
   bool hasNext() {}
 
   uint8_t next() {}
--- steno_keyboard/sd_card_text_test.ino
+++ steno_keyboard/sd_card_text_test.ino
@@ -3,4 +3,21 @@
 
 void testSDCardText() {
   testSuite("SDCardText");
+
+  char fileNameOfTestFile[] = "test";
+  File testFile = SD.open(fileNameOfTestFile, FILE_WRITE);
+  testFile.write("abcdefghijklmnopqrstuvwxyz");
+  testFile.close();
+
+  test("initialize");
+  {
+    uint32_t initialPosition = 0;
+    uint32_t finalPosition = 3;
+    File testFile = SD.open(fileNameOfTestFile);
+    SDCardText text(initialPosition, finalPosition, testFile);
+    testFile.close();
+    assertTrue(true);
+  }
+
+  SD.remove(fileNameOfTestFile);
 }
