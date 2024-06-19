762da28c4774ad032468b56eac9b21f777417d63
mharrison041 <michael.harrison.dev@gmail.com>
2024-06-19 01:02:12 -0600

    Updated interface for Dictionary

--- steno_keyboard/Dictionary.h
+++ steno_keyboard/Dictionary.h
@@ -1,14 +1,7 @@
 #pragma once
+#include "Text.h"
 
 class Dictionary {
 public:
-  virtual bool open() = 0;
-
-  virtual void close() = 0;
-
-  virtual bool seekTextFor(uint8_t steno[]) = 0;
-
-  virtual bool hasNext() = 0;
-
-  virtual uint8_t next() = 0;
+  virtual Text* getTextFor(uint8_t steno[]) = 0;
 };
